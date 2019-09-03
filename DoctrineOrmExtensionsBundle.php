064ee62168eec8aefce5f8eb444dc348898825a3
Mykola Nevmerzhytskyi <mykolan@strivtech.com>
2019-09-03 16:02:13 +0300

    fix

--- DoctrineOrmExtensionsBundle.php
+++ DoctrineOrmExtensionsBundle.php
@@ -8,10 +8,5 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class DoctrineOrmExtensionsBundle extends Bundle
 {
-    public function build(ContainerBuilder $container)
-    {
-        parent::build($container);
 
-        $container->addCompilerPass(new GraphQLResolverPass());
-    }
 }
